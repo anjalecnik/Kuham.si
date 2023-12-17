864a67436f53dcd001dd72c1e02159187f408f9e
Anja Lečnik <anjas.lecnik@gmail.com>
2023-12-17 15:44:13 +0100

    Swagger

--- Backend/src/main/java/si/um/feri/kuham_si/controllers/HisaController.java
+++ Backend/src/main/java/si/um/feri/kuham_si/controllers/HisaController.java
@@ -1,6 +1,8 @@
 package si.um.feri.kuham_si.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import si.um.feri.kuham_si.models.Hisa;
 import si.um.feri.kuham_si.repository.HisaRepository;
@@ -18,8 +20,9 @@ public class HisaController {
     }
 
     @PostMapping
-    public Hisa dodajHiso(@RequestBody Hisa hisa) {
-        return hisaDao.save(hisa);  // hišo, ki jo dobimo preko telesa zahteve, shranimo (save == insert)
+    public ResponseEntity<Hisa> dodajHiso(@RequestBody Hisa hisa) {
+        Hisa hisa_res = hisaDao.save(hisa);  // hišo, ki jo dobimo preko telesa zahteve, shranimo (save == insert)
+        return new ResponseEntity(hisa_res, HttpStatus.CREATED);
     }
 
     @GetMapping // branje
