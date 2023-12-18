6b1e202206fd025a6d82c9516390cca80359b1ae
Anja Lečnik <anjas.lecnik@gmail.com>
2023-12-18 16:21:42 +0100

    Swagger

--- Backend/src/main/java/si/um/feri/kuham_si/Application.java
+++ Backend/src/main/java/si/um/feri/kuham_si/Application.java
@@ -2,24 +2,13 @@ package si.um.feri.kuham_si;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public Docket configureApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage(("si.um.feri.kuham_si"))).build();
-	}
 
 }
