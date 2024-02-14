967a142cd3ac97ad521b0f3062758714ad702368
FallenBanana200 <nik.tisler@gmail.com>
2024-02-14 11:29:57 +0100

    dodajanje entitete kuhar

---
+++ Backend/src/main/java/si/um/feri/kuham_si/models/Kuhar.java
@@ -0,0 +1,63 @@
+package si.um.feri.kuham_si.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Kuhar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String ime;
+    private String priimek;
+    private String uporabniskoIme;
+    private String geslo;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPriimek() {
+        return priimek;
+    }
+
+    public void setPriimek(String priimek) {
+        this.priimek = priimek;
+    }
+
+    public String getGeslo() {
+        return geslo;
+    }
+
+    public void setGeslo(String geslo) {
+        this.geslo = geslo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getUporabniskoIme() {
+        return uporabniskoIme;
+    }
+}
